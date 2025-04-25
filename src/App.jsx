6079344d61d4797f5050864b61d8e3f5a3561233
vasanth <intraintech.vasanth@gmail.com>
2025-04-25 18:55:45 +0530

    Updated tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import React from 'react'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      <div className=''>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
